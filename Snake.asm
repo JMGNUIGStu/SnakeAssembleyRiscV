cae540de39376522896096dd1a96738ea3675d47
Jack <j.mcgirl2@nuigalway.ie>
2020-11-22 15:49:35 +0000

    Test Commit

--- Snake.asm
+++ Snake.asm
@@ -5,8 +5,7 @@
 
 # register allocation
 #  x1-x8 - Used to display snake and food
-#  x9    - Used to get X coordinate for food 
-#  x10   - Used for Y Cordinate for food
+#  x9    - Used to get coordinates for food 
 #  x11   - General Use Register
 #  x12   - general use register 
 #  x13   - general use register
@@ -17,3 +16,8 @@
 #   Use inport (1:0) to control snake
 #   Assert and Deassert 1 to turn snake counterclockwise
 #   Assert and deassert 0 to turn snake clockwise
+
+initialiseRegisters:
+lui x3, 0x00010000
+lui x9, 0x0bcde
+addi x9, x0, 321 
