bbe48cb133140a9dd737bd3d0181c1f1d32fac21
Jack <j.mcgirl2@nuigalway.ie>
2020-11-22 16:57:31 +0000

    Added pseudocode for moving the snake

--- Snake.asm
+++ Snake.asm
@@ -6,8 +6,8 @@
 # register allocation
 #  x1-x8 - Used to display snake and food
 #  x9    - Used to get coordinates for food
-#  x10   - Used Snake location
-#  x11   - General Use Register
+#  x10   - Used to track Snake location + direction
+#  x11   - general use register
 #  x12   - general use register 
 #  x13   - general use register
 #  x14   - general use register 
@@ -15,8 +15,8 @@
 
 # Controls
 #   Use inport (1:0) to control snake
-#   Assert and Deassert 1 to turn snake counterclockwise
-#   Assert and deassert 0 to turn snake clockwise
+#   Assert and Deassert 1 to turn snake clockwise
+#   Assert and deassert 0 to turn snake counterclockwise
 
 initialiseRegisters:
 lui x3, 0x00010000
@@ -41,8 +41,59 @@ decrDelayCountUntil0:              # Delay Loop
  bne   x12, x0,  decrDelayCountUntil0
 
 placeFood:
+# x9(3:0) - Used to get X Co ordinate for food
+# x9(6:4) - Used to get Y Co Ordinate for food
+# x9(7)   - Used to aid RNG
+# add x9, x9(5:3), x9
+# 
+
+
 
 moveSnake:
+# If x10(7)     = 1, Snake in x1
+# If x10(7)     = 2, Snake in x2
+# If x10(7)     = 3, Snake in x3
+# If x10(7)     = 4, Snake in x4
+# If x10(7)     = 5, Snake in x5
+# If x10(7)     = 6, Snake in x6
+# If x10(7)     = 7, Snake in x7
+# If x10(7)     = 8, Snake in x8
+
+# If x10(0)     = 1, Snake Moving Left
+# If rIn(1:0)   = 01 then x10(0) = 4 (Move snake down)
+# If rIn(1:0)   = 10 then x10(0) = 2 (Move snake up)  
+
+# If x10(0)     = 2, Snake Moving Up
+# If rIn(1:0)   = 01 then x10(0) = 1 (Move snake left)
+# If rIn(1:0)   = 10 then x10(0) = 3 (Move snake right)
+
+# If x10(0)     = 3, Snake Moving Right
+# If rIn(1:0)   = 01 then x10(0) = 2 (Move snake up)
+# If rIn(1:0)   = 10 then x10(0) = 4 (Move snake down)  
+
+# If x10(0)     = 4, Snake Moving Down
+# If rIn(1:0)   = 01 then x10(0) = 3 (Move snake right)
+# If rIn(1:0)   = 10 then x10(0) = 1 (Move snake left)  
+
+# Move Left     = sub x10(7), x9
+#                 add, x10(7)(As a register), x10(As a register) e.g. Remove food from row, multiply row by 2, add food back. This prevents the food moving away from the snake
+#                 add x10(7), x9
+
+# Move Right    = sub x10(7), x9
+#                 sub, x10(7)(As a register), x10(As a register) e.g. Divide value of row by 2
+#                 add x10(7), x9
+
+# Move Up       = sub x10(7), x9
+#                 add, x11, x10(7)
+#                 lui x10(7)-1, x11         e.g. Write current row to x11, then move x11 into the row above and clear the starting row
+#                 sub x10(7), x10(7)
+#                 add x10(7), x9
+
+# Move Down     = sub x10(7), x9
+#                 add, x11, x10(7)
+#                 lui x10(7)+1, x11         e.g. Write current row to x11, then move x11 into the row below and clear the starting row
+#                 sub x10(7), x10(7)
+#                 add x10(7), x9
 
 checkFoodEaten:
 
